c17f1ed85614822a4b7d1ae8165988c002e5d55c
ydah <t.yudai92@gmail.com>
2026-09-15 16:02:33 +0900

    Prevent Git maintenance from racing test cleanup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 ENV["MT_NO_PLUGINS"] = "1"
+ENV["GIT_CONFIG_COUNT"] = "1"
+ENV["GIT_CONFIG_KEY_0"] = "maintenance.auto"
+ENV["GIT_CONFIG_VALUE_0"] = "false"
 gem "minitest", "~> 5.0"
 require "minitest/autorun"
 require "tmpdir"
